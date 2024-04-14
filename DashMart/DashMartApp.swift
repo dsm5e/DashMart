5da4da0cbe9fc86b4d0f73e9bc69ba28c5b69722
Vladislav Golyakov <justry24@gmail.com>
2024-04-14 18:37:38 +0300

    Initial Commit

---
+++ DashMart/DashMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashMartApp.swift
+//  DashMart
+//
+//  Created by dsm 5e on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct DashMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
