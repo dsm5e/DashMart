8d117b2a0a76962c59c2a0c29b64b3e045993408
Vanya <whitewolf3310@icloud.com>
2024-04-15 15:54:51 +0300

    add onboarding

---
+++ DashMart/Core/OnboardingScreen/PageIndicator.swift
@@ -0,0 +1,29 @@
+//
+//  PageIndicator.swift
+//  HomeChaleng
+//
+//  Created by Ваня Науменко on 15.04.24.
+//
+
+import SwiftUI
+
+struct PageIndicator: View {
+    var numberOfPages: Int
+    var currentIndex: Int
+
+    var body: some View {
+        HStack(spacing: 10) {
+            ForEach(0 ..< numberOfPages) { index in
+                Rectangle()
+                    .fill(index == currentIndex ? Color.black : Color.gray)
+                    .frame(width: index == currentIndex ? 30 : 10, height: 8)
+                    .cornerRadius(20)
+                    .animation(.default)
+            }
+        }
+    }
+}
+
+#Preview {
+    PageIndicator(numberOfPages: 3, currentIndex: 0)
+}
