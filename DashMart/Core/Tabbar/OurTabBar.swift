70970d402f38cff22de967a9cad9bc270ad7f9c5
Vanya <whitewolf3310@icloud.com>
2024-04-16 16:11:00 +0300

    add file, image and fix error parallelizeBuildables = "YES"

--- DashMart/Core/Tabbar/OurTabBar.swift
+++ DashMart/Core/Tabbar/OurTabBar.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct OurTabBar: View {
     var body: some View {
         TabView {
-            Home().tag("1")
-                .tabItem { Label("Everyone", systemImage: "person.3") }
-            WishList().tag("2")
-                .tabItem { Label("Everyone", systemImage: "person.3") }
-            Manager().tag("3")
-                .tabItem { Label("Everyone", systemImage: "person.3") }
-            Account().tag("4")
-                .tabItem { Label("Everyone", systemImage: "person.3") }
+            Home()
+                .tabItem {
+                    Image("home")
+                    Text("Home")
+                }
+            WishList()
+                .tabItem {
+                    Image("heart")
+                    Text("WishList")
+                }
+            Manager()
+                .tabItem {
+                    Image("paper")
+                    Text("WishList")
+                }
+            Account()
+                .tabItem {
+                    Image("profile")
+                    Text("WishList")
+                }
         }
+        .tint(.black)
     }
 }
 
