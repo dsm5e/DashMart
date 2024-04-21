d26caddef250cbfc9111fabfc6eedbcb6d5efc3a
viktorporch <kavaka08@mail.ru>
2024-04-22 01:46:32 +0300

    add account type switch

---
+++ DashMart/Managers/ManagerService.swift
@@ -0,0 +1,48 @@
+//
+//  ManagerService.swift
+//  DashMart
+//
+//  Created by Victor on 22.04.2024.
+//
+
+import Foundation
+
+final class ManagerService: ObservableObject {
+    @Published var isManager: Bool = false
+    var needPassword: Bool {
+        !wasAuthorizedAsManager
+    }
+    private var wasAuthorizedAsManager = false
+    private var password = "123123"
+    
+    static let shared = ManagerService()
+    
+    private init() { }
+    
+    @discardableResult
+    func enterAsManager(_ password: String?) -> Bool {
+        guard needPassword else {
+            setManager(true)
+            return true
+        }
+        if password == self.password {
+            setManager(true)
+            wasAuthorizedAsManager = true
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logoutAsManager() {
+        setManager(false)
+    }
+    
+    func logout() {
+        wasAuthorizedAsManager = false
+    }
+    
+    private func setManager(_ value: Bool) {
+        isManager = value
+    }
+}
