0bfa21bf3266b453fe15bf4cb8d07550051a206f
Vanya <whitewolf3310@icloud.com>
2024-04-24 13:19:57 +0300

    fix paymentMethod

--- DashMart/Core/HomeScreen/Components/PlaymentMethod.swift
+++ DashMart/Core/HomeScreen/Components/PlaymentMethod.swift
@@ -17,7 +17,7 @@ struct PlaymentMethod: View {
                 Text("**** **** **** 7777")
                 Spacer()
                 Image(systemName: "trash")
-                        
+
             })
             Section(header: Text("Or inputnew cards")) {
                 HStack {
@@ -34,58 +34,61 @@ struct PlaymentMethod: View {
         }
         .bottomSheet(isPresented: $showSheet, contentView: {
             VStack {
-                            Spacer()
-                            Spacer()
-                            Image("imageOk")
-                                .resizable()
-                                .frame(width: 150, height: 150)
-            
-                            Text("Congrats! your payment\nis successfully")
-                                .font(.system(size: 22, weight: .semibold, design: .default))
-                                .foregroundColor(Color(hex: "#393F42"))
-            
-                            Text("Track your order or just chat directly to the seller. Download order summary in down below")
-                                .font(.system(size: 14, design: .default))
-                                .foregroundStyle(Color(hex: "#939393"))
-                                .padding()
+                Image("imageOk")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("Congrats! your payment\nis successfully")
+                    .font(.system(size: 22, weight: .semibold, design: .default))
+                    .foregroundColor(Color(hex: "#393F42"))
+                    .multilineTextAlignment(.center)
+                Spacer()
+                Spacer()
+                Text("Track your order or just chat directly to the\n seller. Download order summary in down below")
+                    .font(.system(size: 14, design: .default))
+                    .foregroundStyle(Color(hex: "#939393"))
+                    .multilineTextAlignment(.center)
+
                 Button {
-                                   showAlertPDF.toggle()
-                               } label: {
-                                   HStack(spacing: 30) {
-                                       Image("file-pdf")
-                                           .resizable()
-                                           .scaledToFill()
-                                           .padding(20)
-                                       Text("Orden invoce")
-                                           .font(.system(size: 10, weight: .semibold, design: .default))
-               
-                                       Image(systemName: "arrow.down.to.line.square")
-                                           .padding()
-                                   }
-                               }
-                               .frame(width: 250, height: 60)
-                               .foregroundStyle(Color(hex: "#939393"))
-                               .buttonBorderShape(.capsule)
-                               .border(Color(hex: "#939393"), width: 2)
-                               .cornerRadius(5)
-                               .alert(isPresented: $showAlertPDF) {
-                                   Alert(title: Text("Download"), message: Text("File loaded successfully"))
-                               }
-               
-                               Spacer()
-               
-                               Button {
-                                   showSheet.toggle()
-                               } label: {
-                                   Text("Continue")
-                                       .font(.system(size: 16, weight: .bold))
-                               }
-                               .frame(width: 350, height: 40)
-                               .foregroundStyle(.white)
-                               .background(Color(hex: "#67C4A7"))
-                               .cornerRadius(5)
-                               Spacer()
-                           }
+//                    showAlertPDF.toggle()
+                } label: {
+                    Image("PDF")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("orden_invoce")
+                        .font(.system(size: 16, design: .default))
+                        .foregroundStyle(Color(hex: "#939393"))
+                    Spacer()
+                    Spacer()
+                    Image(systemName: "arrow.down.to.line.square")
+                        .foregroundStyle(.black)
+                }
+                .padding()
+                .foregroundStyle(Color(hex: "#939393"))
+                .frame(width: 270, height: 50)
+                .background(Color.clear)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(hex: "#939393"), lineWidth: 1)
+                }
+                .alert(isPresented: $showAlertPDF) {
+                    Alert(title: Text("Download"), message: Text("File loaded successfully"))
+                }
+
+                Spacer()
+
+                Button {
+                    showSheet.toggle()
+                } label: {
+                    Text("Continue")
+                        .font(.system(size: 16, weight: .bold))
+                }
+                .frame(width: 350, height: 40)
+                .foregroundStyle(.white)
+                .background(Color(hex: "#67C4A7"))
+                .cornerRadius(5)
+                Spacer()
+            }
         })
     }
 }
