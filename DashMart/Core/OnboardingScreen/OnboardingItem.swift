46a355a33d6caeefe848d32417f223c10009c8ed
Naumenko Ivan <95250432+NaumenkoVanya@users.noreply.github.com>
2024-04-25 12:25:04 +0300

    add new image fix bug (#64)
    Co-authored-by: Vanya <whitewolf3310@icloud.com>

--- DashMart/Core/OnboardingScreen/OnboardingItem.swift
+++ DashMart/Core/OnboardingScreen/OnboardingItem.swift
@@ -17,7 +17,7 @@ struct OnboardingItem: Identifiable {
 }
 
 let onboardingData: [OnboardingItem] = [
-    OnboardingItem(imageName: "onboarding_1", title: "Discover New Styles", description: "Explore our latest collection and discover new styles that suit your taste."),
-    OnboardingItem(imageName: "onboarding_2", title: "Get Inspired", description: "Get inspired by our curated looks and find the perfect outfit for any occasion."),
-    OnboardingItem(imageName: "onboarding_3", title: "Easy Shopping", description: "Enjoy hassle-free shopping with easy navigation, secure payments, and fast delivery.")
+    OnboardingItem(imageName: "onboarding_1_1", title: "Discover New Styles", description: "Explore our latest collection and discover new styles that suit your taste."),
+    OnboardingItem(imageName: "onboarding_2_2", title: "Get Inspired", description: "Get inspired by our curated looks and find the perfect outfit for any occasion."),
+    OnboardingItem(imageName: "onboarding_3_3", title: "Easy Shopping", description: "Enjoy hassle-free shopping with easy navigation, secure payments, and fast delivery.")
 ]
